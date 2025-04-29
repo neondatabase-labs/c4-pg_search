093ddb019b45c2a6e4f510f7a2d593487bd004c0
George MacKerron <george@mackerron.co.uk>
2025-04-29 16:14:10 +0100

    Minor tweaks

--- api/search.ts
+++ api/search.ts
@@ -7,9 +7,10 @@ export const config = {  // Vercel Edge Function config
 
 const title = 'Full-text time machine';
 const resultsPerPage = 20;
+
 const esc = (s: string) => s.replace(/[&<>'"]/g, c => `&${{ '&': 'amp', '<': 'lt', '>': 'gt', "'": 'apos', '"': 'quot' }[c] as string};`);
-const html = (q: string, results: string) => `<!DOCTYPE html>
-  <html>
+const html = (q: string, results: string, t: number) => `<!DOCTYPE html>
+  <html lang="en">
     <head>
       <title>${esc(title)}</title>
       <link rel="stylesheet" href="/style.css">
@@ -24,20 +25,22 @@ const html = (q: string, results: string) => `<!DOCTYPE html>
         ${esc(title)}
       </h1>
       <form method="GET">
-        <p><input type="text" size="80" name="q" value="${esc(q)}"> <input type="submit" value="Search"></p>
+        <p><input type="text" size="50" name="q" value="${esc(q)}"> <input type="submit" value="Search"></p>
       </form>
       ${results}
       <p class="description">
-        Search the web like it’s 2019 and Google never existed.
-        <a href="https://en.wikipedia.org/wiki/Okapi_BM25">BM25</a> full-text search over the 364,868,892 pages
-        of the <a href="https://huggingface.co/datasets/allenai/c4">Colossal Common Crawl Corpus</a> (clean, English-only).
-        Powered by <a href="https://neon.tech/docs/extensions/pg_search">pg_search</a>, <a href="https://neon.tech">Neon</a>
-        and <a href="https://vercel.com/docs/functions">Vercel</a>. <a href="https://github.com/neondatabase-labs/c4-pg_search">Source code</a>.
+        Search the web like it’s 2019 — and <a href="https://web.stanford.edu/class/cs54n/handouts/24-GooglePageRankAlgorithm.pdf">PageRank</a> never existed.
+        This is <a href="https://en.wikipedia.org/wiki/Okapi_BM25">BM25</a> full-text search over the 364,868,892 pages
+        of the <a href="https://huggingface.co/datasets/allenai/c4">Colossal Common Crawl Corpus</a> (clean, English-only),
+        powered by <a href="https://neon.tech/docs/extensions/pg_search">pg_search</a>, <a href="https://neon.tech">Neon</a>
+        and <a href="https://vercel.com/docs/functions">Vercel</a>. Source on <a href="https://github.com/neondatabase-labs/c4-pg_search">GitHub</a>.
       </p>
+      <p class="timing">${t.toFixed(2)} ms</p>
     </body>
   </html>`;
 
 export default async (req: Request) => {
+  const t0 = performance.now();
   const { searchParams } = new URL(req.url);
   const q = searchParams.get('q')?.replace(/[+]/g, ' ').trim() ?? '';
   let offset = Math.max(0, parseInt(searchParams.get('offset') ?? '0', 10));
@@ -45,27 +48,30 @@ export default async (req: Request) => {
 
   if (q !== '') {
     const sql = neon(process.env.DATABASE_URL!);
-    const searchTerms = `"${q.replace(/"/g, ' ').replace(/\s+/g, ' ')}"~2`;
+    const searchTerms = `"${q.replace(/"/g, ' ').replace(/\s+/g, ' ')}"~3`;
 
     const rows = await sql`
-      SELECT id, paradedb.score(id), paradedb.snippet(body), url, body
+      SELECT id, url, body, paradedb.score(id), paradedb.snippet(body)
       FROM pages
       WHERE body @@@ ${searchTerms}
-      ORDER BY paradedb.score(id) DESC, id 
-      OFFSET ${offset} LIMIT ${resultsPerPage + 1}`;
+      ORDER BY paradedb.score(id) DESC, id ASC
+      OFFSET ${offset} LIMIT ${resultsPerPage + 1}`; // ordering by id is for stability
 
     results = `
       <ol start="${offset + 1}">
-        ${rows.slice(0, resultsPerPage).map((result) => `
-          <li class="result">
-            <div class="url">
-              <a href="${result.url}"><span class="domain">${result.url.split('/')[2]}</span><span class="path">/${result.url.split('/').slice(3).join('/')}</span></a> 
-              — <a href="https://web.archive.org/web/*/${result.url}">wayback</a> 
-              / <label><input type="checkbox"><span class="link">text</span><div class="preview"><p>${esc(result.body).replace(/\n/g, '</p><p>')}</p></div></label>
-            </div>
-            <div class="snippet">&hellip;&nbsp;${result.snippet}&nbsp;&hellip;</div>
-          </li>`
-        ).join('\n')}
+        ${rows.slice(0, resultsPerPage).map((result) => {
+          const { url, snippet, body } = result;
+          const [, host, path] = url.match(/[/]([^/]+)(.*)$/) ?? [];
+          return `
+            <li class="result">
+              <div class="url">
+                <a href="${url}"><span class="domain">${host}</span><span class="path">${path}</span></a> 
+                — <a href="https://web.archive.org/web/*/${url}">wayback</a> 
+                / <label><input type="checkbox"><span class="link">text</span><div class="preview"><p>${esc(body).replace(/\n/g, '</p><p>')}</p></div></label>
+              </div>
+              <div class="snippet">&hellip;&nbsp;${snippet}&nbsp;&hellip;</div>
+            </li>`;
+        }).join('\n')}
       </ol>
       <div class="controls">
         ${offset > 0 ? `<a href="?q=${encodeURIComponent(q).replace(/%20/g, '+')}&offset=${offset - resultsPerPage}">&laquo; Prev ${resultsPerPage}</a> &nbsp; ` : ''}
@@ -73,7 +79,7 @@ export default async (req: Request) => {
       </div>`;
   }
 
-  return new Response(html(q, results), {
+  return new Response(html(q, results, performance.now() - t0), {
     headers: { 'Content-Type': 'text/html; charset=utf-8' }
   });
 }
